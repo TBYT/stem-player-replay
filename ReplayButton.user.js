5c3b7447f89f54ff88164385b0b4e3d16b804868
Brian <theblazeyt@gmail.com>
2022-02-27 23:01:55 -0700

    Update ReplayButton.user.js

--- ReplayButton.user.js
+++ ReplayButton.user.js
@@ -4,10 +4,8 @@
 // @version      0.1
 // @description  Replay Button for the stemplayer tracks on their website. Please wait until the site is loaded to press replay button. The website will load the track but takes time to do so (about 2 to 6 seconds), this is a discrepancy on stemplayer.com's part.
 // @author       TBYT
-// @match        https://www.kanyewest.com/connect/stem/tracks
-// @match        https://www.stemplayer.com/connect/stem/tracks
-// @match        https://www.kanyewest.com
-// @match        https://www.stemplayer.com
+// @match        https://www.kanyewest.com/*
+// @match        https://www.stemplayer.com/*
 // @icon         https://www.stemplayer.com/favicon.ico
 // @grant        none
 // ==/UserScript==
@@ -16,14 +14,17 @@
     'use strict';
 
     var count = 0;
-    var ogreplayscript = 'function loadAgain(){var replay = 0;var trcknme = document.getElementsByClassName("track-play__track-info")[0].getElementsByTagName("p")[0].innerHTML;let txtvar1 = document.getElementsByClassName("track-play__play-button")[0].getElementsByClassName("button__text")[0].innerHTML;let numvar2 = parseInt(txtvar1.substring(txtvar1.indexOf(":")+1,txtvar1.indexOf(":")+3));let numvar1 = parseInt(txtvar1.substring(txtvar1.indexOf(":")-2,txtvar1.indexOf(":")));var finaltracklength = (((numvar1*60)+numvar2));setInterval(function() {var plybtn = document.getElementsByClassName("track-play__play-button")[0];let txt1 = plybtn.getElementsByClassName("button__text")[0].innerHTML;let num2 = parseInt(txt1.substring(txt1.indexOf(":")+1,txt1.indexOf(":")+3));let num1 = parseInt(txt1.substring(txt1.indexOf(":")-2,txt1.indexOf(":")));var time = (((num1*60)+num2));if (time==finaltracklength){if (replay==0) {plybtn.click();replay++; }}else { replay = 0; }},0);}';
-    var script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.innerHTML = ogreplayscript;
-    script.id = "replayScript";
-    document.head.appendChild(script);
     setInterval(function()
-                {
+    {
+        if((window.location.href=="https://www.stemplayer.com/connect/stem/tracks") || (window.location.href=="https://www.kanyewest.com/connect/stem/tracks"))
+        {
+            var ogreplayscript = 'function loadAgain(){var replay = 0;var trcknme = document.getElementsByClassName("track-play__track-info")[0].getElementsByTagName("p")[0].innerHTML;let txtvar1 = document.getElementsByClassName("track-play__play-button")[0].getElementsByClassName("button__text")[0].innerHTML;let numvar2 = parseInt(txtvar1.substring(txtvar1.indexOf(":")+1,txtvar1.indexOf(":")+3));let numvar1 = parseInt(txtvar1.substring(txtvar1.indexOf(":")-2,txtvar1.indexOf(":")));var finaltracklength = (((numvar1*60)+numvar2));setInterval(function() {var plybtn = document.getElementsByClassName("track-play__play-button")[0];let txt1 = plybtn.getElementsByClassName("button__text")[0].innerHTML;let num2 = parseInt(txt1.substring(txt1.indexOf(":")+1,txt1.indexOf(":")+3));let num1 = parseInt(txt1.substring(txt1.indexOf(":")-2,txt1.indexOf(":")));var time = (((num1*60)+num2));if (time==finaltracklength){if (replay==0) {plybtn.click();replay++; }}else { replay = 0; }},0);}';
+            var script = document.createElement('script');
+            script.type = 'text/javascript';
+            script.innerHTML = ogreplayscript;
+            script.id = "replayScript";
+            document.head.appendChild(script);
+        }
         try
         {
             if(document.getElementsByClassName("track-play__track-info")[0].getElementsByTagName('p').length!=0)
