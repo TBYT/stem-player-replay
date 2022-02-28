3560c6d95e664c6c43628f856daf539eaad50c51
Brian <theblazeyt@gmail.com>
2022-02-27 23:26:10 -0700

    Update ReplayButton.user.js

--- ReplayButton.user.js
+++ ReplayButton.user.js
@@ -14,23 +14,17 @@
     'use strict';
 
     var count = 0;
-    var insert = 0;
     setInterval(function()
     {
-        if((window.location.href=="https://www.stemplayer.com/connect/stem/tracks") || (window.location.href=="https://www.kanyewest.com/connect/stem/tracks"))
+        if(document.getElementById("replayScript")==null)
         {
-            if (insert==0)
-            {
-                insert++;
                 var ogreplayscript = 'function loadAgain(){var replay = 0;var trcknme = document.getElementsByClassName("track-play__track-info")[0].getElementsByTagName("p")[0].innerHTML;let txtvar1 = document.getElementsByClassName("track-play__play-button")[0].getElementsByClassName("button__text")[0].innerHTML;let numvar2 = parseInt(txtvar1.substring(txtvar1.indexOf(":")+1,txtvar1.indexOf(":")+3));let numvar1 = parseInt(txtvar1.substring(txtvar1.indexOf(":")-2,txtvar1.indexOf(":")));var finaltracklength = (((numvar1*60)+numvar2));setInterval(function() {var plybtn = document.getElementsByClassName("track-play__play-button")[0];let txt1 = plybtn.getElementsByClassName("button__text")[0].innerHTML;let num2 = parseInt(txt1.substring(txt1.indexOf(":")+1,txt1.indexOf(":")+3));let num1 = parseInt(txt1.substring(txt1.indexOf(":")-2,txt1.indexOf(":")));var time = (((num1*60)+num2));if (time==finaltracklength){if (replay==0) {plybtn.click();replay++; }}else { replay = 0; }},0);}';
                 var script = document.createElement('script');
                 script.type = 'text/javascript';
                 script.innerHTML = ogreplayscript;
                 script.id = "replayScript";
                 document.head.appendChild(script);
-            }
         }
-        else { insert = 0; }
         try
         {
             if(document.getElementsByClassName("track-play__track-info")[0].getElementsByTagName('p').length!=0)
