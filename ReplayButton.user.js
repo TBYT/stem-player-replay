df45d92379de6f3dbe2421c2d8503784a8285b46
Brian <theblazeyt@gmail.com>
2022-03-04 01:41:58 -0700

    Update ReplayButton.user.js
    Added replay support for user uploaded tracks.

--- ReplayButton.user.js
+++ ReplayButton.user.js
@@ -17,6 +17,7 @@
     {
         try
         {
+            //og script 1 are for the native stem player tracks.
                     if(document.getElementById("replayScript")==null)
                     {
                             var ogreplayscript = 'function loadAgain(){var replay = 0;let txtvar1 = document.getElementsByClassName("track-play__play-button")[0].getElementsByClassName("button__text")[0].innerHTML;let numvar2 = parseInt(txtvar1.substring(txtvar1.indexOf(":")+1,txtvar1.indexOf(":")+3));let numvar1 = parseInt(txtvar1.substring(txtvar1.indexOf(":")-2,txtvar1.indexOf(":")));var finaltracklength = (((numvar1*60)+numvar2));setInterval(function() {if(document.getElementsByClassName("track-play__play-button").length>0){var plybtn = document.getElementsByClassName("track-play__play-button")[0];let txt1 = plybtn.getElementsByClassName("button__text")[0].innerHTML;let num2 = parseInt(txt1.substring(txt1.indexOf(":")+1,txt1.indexOf(":")+3));let num1 = parseInt(txt1.substring(txt1.indexOf(":")-2,txt1.indexOf(":")));var time = (((num1*60)+num2));if (time==finaltracklength){if (replay==0) {plybtn.disabled=false;plybtn.click();document.getElementById("ReplayButton").disabled=true;document.getElementById("ReplayButtonSpan").innerHTML="[ REPLAY IS ACTIVE ]";replay++; }}else { replay = 0; }}},0);}';
@@ -26,7 +27,18 @@
                             scripttoreplay.id = "replayScript";
                             document.head.appendChild(scripttoreplay);
                     }
-                    if(document.getElementsByClassName("track-play__play-button")[0].value=="")
+            //og script 2 is new, it is for tracks you upload. url: /connect/new/[RANDOMSTRING]
+                    else if(document.getElementById("replayScript2")==null)
+                    {
+                            var ogreplayscript2 = 'function loadAgain2(){var replay = 0;let txtvar1 = document.getElementsByClassName("stem-upload__buttons")[0].getElementsByClassName("button")[0].getElementsByClassName("button__text")[0].innerHTML;let numvar2 = parseInt(txtvar1.substring(txtvar1.indexOf(":")+1,txtvar1.indexOf(":")+3));let numvar1 = parseInt(txtvar1.substring(txtvar1.indexOf(":")-2,txtvar1.indexOf(":")));var finaltracklength = (((numvar1*60)+numvar2));setInterval(function() {if(document.getElementsByClassName("stem-upload__buttons").length>0){var plybtn = document.getElementsByClassName("stem-upload__buttons")[0].getElementsByClassName("button")[0];let txt1 = plybtn.getElementsByClassName("button__text")[0].innerHTML;let num2 = parseInt(txt1.substring(txt1.indexOf(":")+1,txt1.indexOf(":")+3));let num1 = parseInt(txt1.substring(txt1.indexOf(":")-2,txt1.indexOf(":")));var time = (((num1*60)+num2));if (time==finaltracklength){if (replay==0) {plybtn.disabled=false;plybtn.click();document.getElementById("ReplayButton1").disabled=true;document.getElementById("ReplayButtonSpan1").innerHTML="[ REPLAY IS ACTIVE ]";replay++; }}else { replay = 0; }}},0);}';
+                            var scripttoreplay2 = document.createElement('script');
+                            scripttoreplay2.type = 'text/javascript';
+                            scripttoreplay2.innerHTML = ogreplayscript2;
+                            scripttoreplay2.id = "replayScript2";
+                            document.head.appendChild(scripttoreplay2);
+                    }
+            //for native stem player tracks
+                    if(document.getElementsByClassName("track-play__play-button").length>0)
                     {
                         //count condition in place to generate only 1 button each time we're on a track.
                         if (count == 0)
@@ -60,7 +72,41 @@
                                 document.getElementsByClassName("track-play__play-button")[0].disabled = true;
                             }
                         }
-                        
+                    }
+            //for user uploaded track.
+                    else if(document.getElementsByClassName("stem-upload__buttons").length>0)
+                    {
+                        if (count == 0)
+                        {
+                            count++;
+                            var button = document.createElement('button');
+                                button.addEventListener("click", function () {
+                                    loadAgain2();
+                                });
+                                button.id = 'ReplayButton1';
+                                button.className = 'button button--text';
+                                button.type = 'button';
+                                document.getElementsByClassName("stem-upload")[0].appendChild(button);
+
+                                var span = document.createElement('span');
+                                span.className = 'button__text';
+                                span.innerHTML = '[ REPLAY BY TBYT ]';
+                                span.id = 'ReplayButtonSpan1';
+                                document.getElementById('ReplayButton1').appendChild(span);
+                        }
+                        let plytxt = document.getElementsByClassName("stem-upload__buttons")[0].getElementsByClassName("button")[0].getElementsByClassName("button__text")[0].innerHTML;
+                        if(((parseInt(plytxt.substring(plytxt.indexOf(":")+1,plytxt.indexOf(":")+3)))+(parseInt(plytxt.substring(plytxt.indexOf(":")-2,plytxt.indexOf(":"))))==0)||document.getElementById("ReplayButtonSpan1").innerHTML=="[ REPLAY IS ACTIVE ]")
+                        {
+                                document.getElementById("ReplayButton1").disabled=true;
+                        }
+                        else 
+                        {
+                            document.getElementById("ReplayButton1").disabled=false; 
+                            if(document.getElementById("ReplayButtonSpan1").innerHTML=="[ REPLAY BY TBYT ]")
+                            {
+                                document.getElementsByClassName("stem-upload__buttons")[0].getElementsByClassName("button")[0].disabled = true;
+                            }
+                        }
                     }
         }
         //Track is not in visibility.
